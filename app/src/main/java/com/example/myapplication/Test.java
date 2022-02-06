ef43e370d904114b735cfbb9fc4ed8bf42cd0e03
Marin Tenzherski <marin_mm@abv.bg>
2022-02-07 00:02:23 +0200

    test commit

--- app/src/main/java/com/example/myapplication/Test.java
+++ app/src/main/java/com/example/myapplication/Test.java
@@ -1,5 +1,10 @@
 package com.example.myapplication;
 
 public class Test {
-    /// testwqewqe
+    /// testwqewqet
+    // test 12312
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
