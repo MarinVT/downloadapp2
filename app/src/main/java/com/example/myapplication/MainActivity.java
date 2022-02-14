ca30562df4fc2886f9e3266421456828f210bfc9
Marin Tenzherski <marin_mm@abv.bg>
2022-02-14 23:32:08 +0200

    Added top10 and top20 buttons and refresh functinality.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -23,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private ListView listViewApp;
+    private String feedURL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml";
+    private int feedLimit = 10;
+
+    private String feedCacheURl = "INVALIDATED";
+    public static final String STATE_URL = "feedURL";
+    public static final String STATE_LIMIT = "feedLimit";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,22 +37,81 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         listViewApp = (ListView) findViewById(R.id.xmlListView);
 
-        Log.d(TAG, "onCreate: stating with AsyncTask");
-        DownloadData downloadData = new DownloadData();
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
-        Log.d(TAG, "onCreate: done");
+        if (savedInstanceState != null) {
+            feedURL = savedInstanceState.getString(STATE_URL);
+            feedLimit = savedInstanceState.getInt(STATE_LIMIT);
+        }
 
+        downloadURL(String.format(feedURL, feedLimit));
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.feeds_menu, menu);
+        if (feedLimit == 10) {
+            menu.findItem(R.id.menu10).setChecked(true);
+        } else {
+            menu.findItem(R.id.menu25).setChecked(true);
+        }
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.menuFree:
+                feedURL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml";
+                break;
+            case R.id.menuPaid:
+                feedURL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=%d/xml";
+                break;
+            case R.id.menuSongs:
+                feedURL = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=%d/xml";
+                break;
+            case R.id.menu10:
+            case R.id.menu25:
+                if (!item.isChecked()) {
+                    item.setChecked(true);
+                    feedLimit = 35 - feedLimit;
+                    Log.d(TAG, "onOptionsItemSelected: " + item.getTitle() + " setting feedLimit to " + feedLimit);
+                } else {
+                    Log.d(TAG, "onOptionsItemSelected: " + item.getTitle() + " feedLimit unchanged!");
+                }
+                break;
+            case R.id.menuRefresh:
+                feedCacheURl = "Invalid";
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+        downloadURL(String.format(feedURL, feedLimit));
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putString(STATE_URL, feedURL);
+        outState.putInt(STATE_LIMIT, feedLimit);
+        super.onSaveInstanceState(outState);
+    }
+
+    private void downloadURL(String feedURL) {
+
+        if(!feedURL.equalsIgnoreCase(feedCacheURl)) {
+
+            Log.d(TAG, "downloadURL: stating with AsyncTask");
+            DownloadData downloadData = new DownloadData();
+            downloadData.execute(feedURL);
+
+            feedCacheURl = feedURL;
+
+            Log.d(TAG, "downloadURL: done");
+        } else {
+            Log.d(TAG, "downloadURL: URL NOT CHANGED!!! <------");
+        }
     }
 
     private class DownloadData extends AsyncTask<String, Void, String> {
